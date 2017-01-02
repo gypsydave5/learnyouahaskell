8637c7ab5ea8349f4900e47142763ab673f7c6ce
David Wickes <dave.wickes@gmail.com>
2017-01-02 20:33:45 +0000

    some changes

--- moreIO/showArgs.hs
+++ moreIO/showArgs.hs
@@ -2,9 +2,11 @@ import System.Environment
 import Data.List
 
 main = do
-    args <- getArgs
+    (x:xs) <- getArgs
     progName <- getProgName
+    putStrLn "The first arg is:"
+    putStrLn x
     putStrLn "The args are:"
-    mapM_ putStrLn args
+    mapM_ putStrLn xs
     putStrLn "The file name is:"
     putStrLn progName
