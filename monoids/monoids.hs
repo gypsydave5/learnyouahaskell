6879f6f708dca75d12820696d07920808a059aeb
David Wickes <dave.wickes@gmail.com>
2017-01-03 23:07:03 +0000

    mmmore mmmonoids

--- monoids/monoids.hs
+++ monoids/monoids.hs
@@ -26,12 +26,11 @@
 -- 1 is id wrt *; [] is id wrt ++
 --
 -- Behold
-
-class Monoid' m where
-    mempty':: m -- a polymorphic constant - the mempty value for the type. The identity value for a particular monoid
-    mappend' :: m -> m -> m
-    mconcat' :: [m] -> m
-    mconcat' = foldr mappend' mempty' -- default implementation of mconcat based on the other two functions
+class Monoid' m  where
+  mempty' :: m -- a polymorphic constant - the mempty value for the type. The identity value for a particular monoid
+  mappend' :: m -> m -> m
+  mconcat' :: [m] -> m
+  mconcat' = foldr mappend' mempty' -- default implementation of mconcat based on the other two functions
 
 --
 -- Monoid Laws
@@ -43,10 +42,10 @@ class Monoid' m where
 --
 -- note that mononoids are not necessarily _commutative_
 -- i.e. a mappend b /= b mappend a
-
 instance Monoid' [a] where
-    mempty' = []
-    mappend' = (++)
+  mempty' = []
+  mappend' = (++)
+
 -- get mconcat' for free - kinda like a method on a superclass (but not)
 --
 -- So here's a thing - the monoid of (+, Int) is different for the monoid of
@@ -56,39 +55,107 @@ instance Monoid' [a] where
 --
 -- But we obviously need a way to determine which monoid we're using - and yet
 -- again it's newtype to the rescue
+newtype Product' a = Product'
+  { getProduct' :: a
+  } deriving (Eq, Ord, Read, Show, Bounded)
 
-newtype Product' a = Product' { getProduct' :: a }
-    deriving (Eq, Ord, Read, Show, Bounded)
+instance Num a =>
+         Monoid' (Product' a) where
+  mempty' = Product' 1
+  mappend' (Product' x) (Product' y) = Product' (x * y)
 
-instance Num a => Monoid' (Product' a) where
-    mempty' = Product' 1
-    mappend' (Product' x) (Product' y) = Product' (x * y)
+newtype Sum' a = Sum'
+  { getSum' :: a
+  } deriving (Eq, Ord, Read, Show, Bounded)
 
-newtype Sum' a = Sum' { getSum' :: a }
-    deriving (Eq, Ord, Read, Show, Bounded)
-
-instance Num a => Monoid' (Sum' a) where
-    mempty' = Sum' 0
-    mappend' (Sum' x) (Sum' y) = Sum' (x + y)
+instance Num a =>
+         Monoid' (Sum' a) where
+  mempty' = Sum' 0
+  mappend' (Sum' x) (Sum' y) = Sum' (x + y)
 
 -- other monoids include the type Bool with the functions || and &&
 -- think about it - if you give (|| False) any Bool you'll get that Bool (True
 -- or False), and the same goes for (&& True). So the monoids are (False, ||) and
 -- (True, &&)
-
-newtype Any' = Any' { getAny' :: Bool }
-    deriving (Eq, Ord, Read, Show, Bounded)
+newtype Any' = Any'
+  { getAny' :: Bool
+  } deriving (Eq, Ord, Read, Show, Bounded)
 
 instance Monoid' Any' where
-    mempty' = Any' False
-    mappend' (Any' a) (Any' b) = Any' (a || b)
+  mempty' = Any' False
+  mappend' (Any' a) (Any' b) = Any' (a || b)
 
-newtype All' = All' { getAll' :: Bool }
-    deriving (Eq, Ord, Read, Show, Bounded)
+newtype All' = All'
+  { getAll' :: Bool
+  } deriving (Eq, Ord, Read, Show, Bounded)
 
 instance Monoid' All' where
-    mempty' = All' True
-    mappend' (All' a) (All' b) = All' (a && b)
+  mempty' = All' True
+  mappend' (All' a) (All' b) = All' (a && b)
+  --
+  -- getAll' $ mappend' mempty' mempty' ==> True
+  -- getSum'.mconcat'.map Sum' $ [1,2,3,4,5] ==> 15
 
--- getAll' $ mappend' mempty' mempty' ==> True
--- getSum'.mconcat'.map Sum' $ [1,2,3,4,5] ==> 15
+--
+--
+-- You can do it for the Ordering type too
+instance Monoid' Ordering where
+  mempty' = EQ
+  mappend' LT _ = LT
+  mappend' EQ y = y
+  mappend' GT _ = GT
+
+-- this is hard, but it works - the left hand side ordering has priority over
+-- the right hand ordering. Consider the following
+--
+lengthCompare :: String -> String -> Ordering
+lengthCompare x y =
+  let a = compare (length x) (length y)
+      b = compare x y
+  in if a == EQ
+       then b
+       else a
+
+lengthCompare' :: String -> String -> Ordering
+lengthCompare' x y = mappend (compare (length x) (length y)) (compare x y)
+
+-- sorting first by length, then by alphabetical order - you could stack
+-- together a series of comparisons in order of importance - think of the use
+-- for sorting.
+-- extension becomes trivial
+myCompare :: String -> String -> Ordering
+myCompare x y =
+  mconcat'
+    [compare (length x) (length y), compare (vowels x) (vowels y), compare x y]
+  where
+    vowels = length . filter (`elem` "aeiou")
+
+-- Maybe as a monoid
+instance Monoid' a =>
+         Monoid' (Maybe a) where
+  mempty' = Nothing
+  mappend' Nothing m = m
+  mappend' m Nothing = m
+  mappend' (Just m1) (Just m2) = Just (mappend' m1 m2)
+
+-- iff a is a monoid itself
+newtype First' a = First'
+  { getFirst' :: Maybe a
+  } deriving (Eq, Ord, Read, Show)
+
+instance Monoid' (First' a) where
+  mempty' = First' Nothing
+  mappend' (First' (Just x)) _ = First' (Just x)
+  mappend' (First' Nothing) x = x
+  -- so now you always get the first one unless it's nothing
+  -- hey and guess what
+  --
+
+newtype Last' a = Last'
+  { getLast' :: Maybe a
+  } deriving (Eq, Ord, Read, Show)
+
+instance Monoid' (Last' a) where
+  mempty' = Last' Nothing
+  mappend' _ (Last' (Just x)) = Last' (Just x)
+  mappend' x (Last' Nothing) = x
