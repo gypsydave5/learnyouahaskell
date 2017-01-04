b8cea8f2011cd8a5d97a852d4d73ff1acc5e4e2d
David Wickes <dave.wickes@gmail.com>
2017-01-04 22:58:17 +0000

    foldable with monoids and stuff

--- monoids/foldable.hs
+++ monoids/foldable.hs
@@ -1,3 +1,5 @@
+import Data.Monoid
+
 data Tree a
   = EmptyTree
   | Node a
@@ -14,3 +16,17 @@ testTree =
     5
     (Node 3 (Node 1 EmptyTree EmptyTree) (Node 6 EmptyTree EmptyTree))
     (Node 9 (Node 8 EmptyTree EmptyTree) (Node 10 EmptyTree EmptyTree))
+
+-- enjoy all these lovely foldy things
+allTheNodes = foldMap (: []) testTree
+
+anyGtFifteen = getAny $ foldMap (Any . (> 15)) testTree
+
+anyThree = getAny $ foldMap (Any . (== 3)) testTree
+
+addEmUp = getSum $ foldMap Sum testTree
+
+timesEmAll = getProduct $ foldMap Product testTree
+
+length' t = appEndo (foldMap (\_ -> Endo (+ 1)) t) 0
+-- don't worry abount this bit of madness...
